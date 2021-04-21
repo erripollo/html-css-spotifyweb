bfcf6ce97dcf03f15b2ec1bd95fa08322f7eed16
erripollo <pollone.enrico@gmail.com>
2021-04-21 16:21:22 +0200

    Create main.js file

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+//write line
+document.writeln('www.spotify.com');
+
+//alert
+alert('Periodo di prova gratuito scaduto');
+
+//getElementById
+document.getElementById('title_section_1').innerHTML='Più ascoltati'
